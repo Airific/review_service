aacfbbea4a90423803cfa6d54021608efa397254
Alissa Colascione <acolascione27@gmail.com>
2020-09-24 19:06:50 -0400

    working get request in front end, randomId function to generate randomId for now

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      reviews: []
+    };
+    this.getReviewsById = this.getReviewsById.bind(this);
+    this.randomId = this.randomId.bind(this);
+  }
+
+  componentDidMount() {
+    const locId = this.randomId();
+    this.getReviewsById(locId);
+  }
+
+  getReviewsById(id) {
+    axios.get(`/listings/reviews/${id}`)
+      .then((res) => {
+        this.setState({
+          reviews: res.data
+        });
+      })
+      .catch(console.log);
+  }
+
+  randomId() {
+    const id = Math.floor(Math.random() * 100);
+    console.log(id, "2");
+    return id;
   }
 
   render() {
     // something
     return (
-      <div>Hello React is working!</div>
+    <div>Hello React is working!{this.state.reviews.length}</div>
     );
   }
 }
