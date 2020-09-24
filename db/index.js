94692c8ca76984fb2b772dfffec2b7cc585394c0
Alissa Colascione <acolascione27@gmail.com>
2020-09-24 13:24:50 -0400

    db seeder working with fake data and s3 images

---
+++ db/index.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const faker = require('faker');
+const { images } = require('./images.js');
+
+mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error'));
+
+db.once('open', () => {
+  console.log('Successfully connected to MongoDB!');
+});
+
+db.dropCollection('reviews', (err) => {
+  if (err) {
+    console.log('error dropping collection ', err);
+  } else {
+    console.log('collection dropped');
+  }
+});
+
+const reviewSchema = new mongoose.Schema({
+  loc_id: Number,
+  name: String,
+  photo: String,
+  date: String,
+  text: String,
+  cleanliness: Number,
+  communication: Number,
+  check_in: Number,
+  accuracy: Number,
+  location: Number,
+  value: Number,
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+const reviews = [];
+// text: faker.lorem.sentences(),
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < 2000; i++) {
+  const fakeReview = new Review({
+    loc_id: faker.random.number({ min: 1, max: 100 }),
+    name: faker.name.firstName(),
+    photo: faker.random.arrayElement(images),
+    date: faker.date.month(),
+    text: faker.lorem.sentences(faker.random.number({ min: 2, max: 4 })),
+    cleanliness: faker.random.number({ min: 1, max: 5 }),
+    communication: faker.random.number({ min: 1, max: 5 }),
+    ccheck_in: faker.random.number({ min: 1, max: 5 }),
+    caccuracy: faker.random.number({ min: 1, max: 5 }),
+    location: faker.random.number({ min: 1, max: 5 }),
+    value: faker.random.number({ min: 1, max: 5 }),
+
+  });
+  reviews.push(fakeReview);
+}
+
+Review.insertMany(reviews)
+  .then(() => {
+    console.log('Data inserted');
+  })
+  .catch((err) => {
+    console.log('error inserting data: ', err);
+  });
+
+module.exports = db;
