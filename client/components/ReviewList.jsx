6125d3b82ee19c615ec836936c62c983d4731375
Alissa Colascione <acolascione27@gmail.com>
2020-09-28 13:48:38 -0400

    created containers for ccomponents using styled-components and styled reviewList and reviews indivulally

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -1,14 +1,36 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Styled from 'styled-components';
 // eslint-disable-next-line import/extensions
 import Review from './Review.jsx';
 
+const ListGrid = Styled.div`
+  display: flex;
+  flex-flow: row wrap;
+
+  justify-content: space-around;
+
+`;
+
+const Nested = Styled.div`
+  width: 470px;
+  margin-bottom: 20px;
+
+`;
+
 const ReviewList = ({ reviews = [] }) => {
-  const list = reviews.map((review) => <Review review={review} key={review._id} />);
+  const list = reviews.slice(0, 6).map(
+    (review) => (
+      <Nested>
+        <Review review={review} key={review._id} />
+      </Nested>
+    ),
+  );
   return (
-    <ul>
+    <ListGrid>
       {list}
-    </ul>
+    </ListGrid>
   );
 };
 
